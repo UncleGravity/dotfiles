10fde31fbe49c90598aa0e4c3a6ec638ed58f276
UncleGravity <viera.tech@gmail.com>
2024-07-01 19:12:01 +0800

    .

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ## If you want home manager to "see" a git submodule
 sudo nixos-rebuild switch --flake ".?submodules=1#default" -v
+
+# For the Raspberry Pi
+1. Install Nix (Determinate Installer)
+2. Git clone this repo
+3. cd into repo
+4. run `home-manager switch --flake .#pi`
--- pi/home.nix
+++ pi/home.nix
@@ -17,56 +17,105 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    pkgs.hello
+  home.packages = with pkgs; [
+    tmux
+    zellij
+    tree
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    # goodies
+    fastfetch
+    btop
+    ripgrep # better grep
+    bat # better cat
+    eza # better ls/tree
+    fd # better find
+    yazi
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    # alacritty
+    wezterm
+
+    pipx
+
+    # USB Stuff
+    cyme
+    usbutils
+
+    # Zsh stuff
+    meslo-lgs-nf # Nerd Font for powerlevel10k
+
+    llm # https://github.com/simonw/llm
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  # ---------------------------------------------
+  # Programs
+  # ---------------------------------------------
+
+  programs.zsh = {
+    enable = true;
+    dotDir = ".config/zsh";
+    # enableCompletion = true;
+    # autosuggestion.enable = true;
+    # syntaxHighlighting.enable = true;
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/pi/etc/profile.d/hm-session-vars.sh
-  #
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.wezterm = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraConfig = ''
+      set number relativenumber
+    '';
+
+    # OR
+    #extraConfig = lib.fileContents ../path/to/your/init.vim;
+  };
+
+  programs.git = {
+    enable = true;
+    
+    # Set Git global configurations
+    userName = "UncleGravity";
+    userEmail = "viera.tech@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+
+  # ---------------------------------------------
+  # DOTFILES
+  # ---------------------------------------------
+  home.file = {
+    ".config/zsh" = {
+      source = ../dotfiles/zsh; 
+      recursive = true; # Allow the directory to be writable, since zplug will create files in it
+    };
+    ".config/wezterm".source = ../dotfiles/wezterm;
+    ".config/alacritty".source = ../dotfiles/alacritty;
+    ".config/tmux" = {
+      source = ../dotfiles/tmux;
+      recursive = true; # This allows the directory to be writable, since tpm will create files in it
+      executable = true; # Not sure if necessary w/e
+    };
+    ".config/zellij".source = ../dotfiles/zellij;
+  };
+
+
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
