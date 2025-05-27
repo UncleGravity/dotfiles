6ea04f685f584c067af2cba3efc78ce8b000aebe
UncleGravity <viera.tech@gmail.com>
2025-05-27 13:07:31 +0800

    docker

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -1,39 +1,31 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, username, ... }:
 
 {
-
-  # ---------------------------------------------------------------------------
-  # PODMAN CONFIG
-  
-  # Enable common container config files in /etc/containers
-  # virtualisation.containers.enable = true;
-
-  # virtualisation = {
-  #   podman = {
-  #     enable = true;
-
-  #     # Create a `docker` alias for podman, to use it as a drop-in replacement
-  #     dockerCompat = true;
-
-  #     # Required for containers under podman-compose to be able to talk to each other.
-  #     defaultNetwork.settings.dns_enabled = true;
-  #   };
-  # };
-
   # ---------------------------------------------------------------------------
   # DOCKER
+  # Root and Rootless can coexist, select which one by running docker with or without sudo
 
+  # ---------------------------------------------------------------------------
+  # Allow docker to access the network (to avoid port forwarding manually... I think)
+  networking.firewall.trustedInterfaces = [ "docker0" ];
+
+  # ---------------------------------------------------------------------------
   # Root Docker
-  # virtualisation.docker.enable = true;
-  # users.users.${username}.extraGroups = [ "docker" ];
+  virtualisation.docker.enable = true;
+  users.users.${username}.extraGroups = [ "docker" ];
 
+  # ---------------------------------------------------------------------------
   # Rootless Docker
-  # Creates rootless docker service, can exist alongside root docker
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true; # will force docker to use rootless context
   };
 
-  # Enable docker service, rootless service not enabled by default
+  # Enable rootless docker service, it's not enabled by default
   systemd.user.services."docker".enable = true;
+
+  # Rootless Docker won't have permission to use ports below 1024 by default
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_unprivileged_port_start" = 80;
+  };
 }
