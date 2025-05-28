c91ed231718a0a85be12d3f55dff8c9fecd63144
UncleGravity <viera.tech@gmail.com>
2025-05-28 16:52:46 +0800

    tailscale: advertise exit node and subnet

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -1,6 +1,10 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
+
+  environment.systemPackages = [ pkgs.tailscale ];
+  networking.firewall.trustedInterfaces = [ "tailscale0" ];
+
   sops.secrets."tailscale/authkey" = {
     mode = "0600"; # Only root can read the auth key
   };
@@ -8,14 +12,14 @@
   services.tailscale = {
     enable             = true;
     authKeyFile        = config.sops.secrets."tailscale/authkey".path;  
-    openFirewall       = true;              # allow the Tailscale UDP port through the firewall
-    # useRoutingFeatures = "both";          # enable subnet-router & exit-node roles 
-    # extraUpFlags       = [
-    #   "--advertise-exit-node"             # if you want this server to be an exit node
-    #   "--accept-routes"                   # if you want it to accept subnet routes
-    # ];
-  };
-
-  # Let your firewall trust the tailscale0 interface
-  networking.firewall.trustedInterfaces = [ "tailscale0" ];  
+    openFirewall       = true;            # allow the Tailscale UDP port through the firewall
+    useRoutingFeatures = "both";          # enable subnet-router & exit-node roles 
+    extraUpFlags       = [
+      "--advertise-exit-node"
+      "--reset"                           # reset unspecified settings to default values.
+    ];
+    extraSetFlags = [
+      "--advertise-routes=192.168.1.0/24"
+    ];
+  };  
 }
