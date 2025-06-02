f0b1cca6e7741c7c3393152c9dd8de90f7741b0b
UncleGravity <viera.tech@gmail.com>
2025-06-02 21:59:28 +0800

    faster samba

--- modules/nixos/samba.nix
+++ modules/nixos/samba.nix
@@ -1,7 +1,7 @@
 { lib, config, pkgs, hostname, username, ... }:
 {
   environment.systemPackages = [ config.services.samba.package ];
-  
+
   # SOPS secret for samba password
   sops.secrets."samba/password" = {
     mode = "0600";
@@ -22,20 +22,56 @@
         "invalid users" = [ "root" ];
         "guest account" = "nobody";
         "map to guest" = "bad user";
+
+        # Logging
+        "log level" = "1";
+        "log file" = "/var/log/samba/log.%m";
+        "max log size" = "1000";
+
+        # Apple
+        "vfs objects" = "catia fruit streams_xattr";
+        "fruit:aapl" = "yes";
+        "fruit:time machine" = "yes";
+        "fruit:time machine max size" = "0";
+        "fruit:time machine max num files" = "0";
+        "fruit:time machine max num folders" = "0";
+
+        # Performance
+        "socket options" = "TCP_NODELAY IPTOS_LOWDELAY";
+        "use sendfile" = "yes";
+        "read raw" = "yes";
+        "write raw" = "yes";
+        "min receivefile size" = "16384";
+        "aio read size" = "16384";
+        "aio write size" = "16384";
+        "server multi channel support" = "yes";
+
+        # Additional performance tweaks
+        "deadtime" = "30";
+        "getwd cache" = "yes";
+        "lpq cache time" = "30";
+        "max smbd processes" = "0";  # 0 = unlimited
+
+        # # Disable unused services
+        # "load printers" = "no";
+        # "printing" = "bsd";
+        # "printcap name" = "/dev/null";
+        # "disable spoolss" = "yes";
       };
+      # Main NAS share for general access
       nas = {
         path = "/nas";
+        "comment" = "All the NAS folders";
         "preserve case" = "yes";
         "short preserve case" = "yes";
         "browseable" = "yes";
         "writeable" = "yes";
-        "read only" = "no";
         "guest ok" = "no";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "valid users" = "${username}";
-        "fruit:aapl" = "yes";
-        "vfs objects" = "catia fruit streams_xattr";
+        "create mask" = "0664";    # Group-writable files
+        "directory mask" = "0775"; # Group-writable directories
+        "inherit acls" = "yes";
+        "veto files" = "/._*/.DS_Store/.Thumbs.db/.Trashes";
+        "valid users" = "${username} @share";
       };
     };
   };
@@ -77,7 +113,7 @@
   # ---------------------------------------------------------------------------
   # Set samba password
   system.activationScripts.sambaUser = {
-    deps = [ 
+    deps = [
       "users"         # Wait for users to be available
       "setupSecrets"  # Wait for secrets to be available
     ];
@@ -85,10 +121,10 @@
       # Wait for samba to be ready
       ${pkgs.systemd}/bin/systemctl is-active --quiet samba-smbd || ${pkgs.systemd}/bin/systemctl start samba-smbd
       sleep 2
-      
+
       # Read password from secure location
       smb_password=$(cat "${config.sops.secrets."samba/password".path}")
-      
+
       # Add/update user
       echo -e "$smb_password\n$smb_password\n" | ${lib.getExe' pkgs.samba "smbpasswd"} -a -s ${username}
     '';
