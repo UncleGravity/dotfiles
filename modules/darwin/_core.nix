f9a757faa74b7f4f3d424a01ec6626b3ed726caa
UncleGravity <viera.tech@gmail.com>
2025-07-09 15:02:51 +0800

    always-allow-substitutes (binary cache)
    Is this necessary? Who knows. What does it do? I don't know.

--- modules/darwin/_core.nix
+++ modules/darwin/_core.nix
@@ -59,6 +59,7 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
   ];
+  nix.settings.always-allow-substitutes = true;
 
   # Flake gang
   nix.channel.enable = false;
--- modules/nixos/_core.nix
+++ modules/nixos/_core.nix
@@ -26,6 +26,7 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
   ];
+  nix.settings.always-allow-substitutes = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
