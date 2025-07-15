fb3de5b12c26a998d11d078c46b0038ec95f671c
UncleGravity <viera.tech@gmail.com>
2025-07-15 21:28:52 +0800

    refactor(aichat): convert from dotfiles to nix module based configuration

---
+++ modules/home/aichat.nix
@@ -0,0 +1,28 @@
+{...}: {
+  programs.aichat = {
+    enable = true;
+    settings = {
+      model = "claude";
+      stream = true;
+      save = true;
+      keybindings = "emacs";
+      editor = null;
+      wrap = "no";
+      wrap_code = false;
+      clients = [
+        {type = "openai";}
+        {type = "claude";}
+        {
+          type = "openai-compatible";
+          name = "ollama";
+          api_base = "http://localhost:11434/v1";
+          models = [
+            {name = "gemma2:27b";}
+            {name = "deepseek-coder-v2";}
+            {name = "hf.co/bartowski/Qwen2.5-Coder-32B-Instruct-GGUF:Q6_K_L";}
+          ];
+        }
+      ];
+    };
+  };
+}
