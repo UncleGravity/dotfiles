b008557da54f137c8b2b54cd6ee233e56611ffcd
UncleGravity <viera.tech@gmail.com>
2025-07-18 18:37:55 +0800

    feat(pkgs): add wrapper-manager | introduce "my" namespace for custom packages
    - Introduces `wrapper-manager`
    - Initial wrappers: `helix` w/ config.toml + "hello" (as a test)
    - Expose all scripts + wrappers under "packages"
    - ie. Callable with eg `nix run .#wrapped.hello`
    - Added custom packages to nixpkgs overlays, under "my"
    - ie. [ pkgs.my.wrapped.hello namespace ]

---
+++ packages/wrappers/default.nix
@@ -0,0 +1,33 @@
+{ pkgs
+, wrapper-manager
+}:
+
+# -----------------------------------------------------------------------------
+# All wrapper-manager modules go here.
+# https://github.com/viperML/wrapper-manager/
+#
+# 1.  `modules` is a hand-maintained list of files, each exporting a
+#     wrapper-manager module (attrset).
+# 2.  We call `wrapper-manager.lib` once with that list.
+# 3.  We return:
+#       • every wrapped derivation under its name   (e.g. `hello`)
+#       • a `build` attrset containing { packages, toplevel, … } for bulk use
+#
+# Example usage after importing this attrset as `wrapped`:
+#
+#   nix run .#wrapped.hello
+#   environment.systemPackages = builtins.attrValues wrapped.build.packages;
+# -----------------------------------------------------------------------------
+let
+
+  wmEval = wrapper-manager.lib {
+    inherit pkgs;
+    modules = [
+      ./hello.nix
+      ./helix
+      # add more modules here (./git.nix, ./fzf.nix, …)
+    ];
+  };
+in
+  # Forward individual wrappers AND bulk helpers (`build` attrset).
+  wmEval.config.build.packages // { build = wmEval.config.build; }
---
+++ packages/wrappers/helix/default.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+{
+  # Create a wrapped version of `hx` that always uses our custom config
+  wrappers.helix = {
+    basePackage = pkgs.helix;
+    appendFlags = [ "--config" ./config.toml ];
+  };
+}
