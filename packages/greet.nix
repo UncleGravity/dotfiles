881dd95e9816a8dbf82939d28c461e170493beb0
UncleGravity <viera.tech@gmail.com>
2025-07-22 14:12:26 +0800

    testing "pkgs.writeShellApplication"

---
+++ packages/greet.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+pkgs.writeShellApplication {
+  name           = "greet";
+  runtimeInputs  = [ pkgs.coreutils ];
+  text = ''
+    #!${pkgs.runtimeShell}
+    echo "Hello from Nix shell script!"
+  '';
+}
