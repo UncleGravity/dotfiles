0f16cf2d1c7489c9efd7eef84d6466d5c19a4442
UncleGravity <viera.tech@gmail.com>
2025-07-23 11:36:48 +0800

    ci: idk

--- modules/common/sops.nix
+++ modules/common/sops.nix
@@ -19,6 +19,11 @@ in {
     defaultSopsFile = "${secretsDir}/secrets.yaml";
     validateSopsFiles = true;
     # validateSopsFiles = builtins.pathExists "/etc/ssh/ssh_host_ed25519_key"; # Only validate when SSH key exists (not in CI)
+    environment = {
+      # SOPS_AGE_KEY_CMD = ''su - ${username} -c "op item get \"master-ssh-key\" --field \"private-key-age\" --reveal"'';
+      # SOPS_AGE_KEY_CMD = "${secretsDir}/keys.sh";
+      # SOPS_AGE_SSH_PRIVATE_KEY_FILE = "${config.users.users.${username}.home}/.ssh/id_ed25519";
+    };
 
     secrets = lib.mkMerge [
       # Work secrets, only on macOS machines
