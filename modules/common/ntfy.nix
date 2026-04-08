491d77bd94caf2245e1a9ce8b272ff92dcfdebec
UncleGravity <viera.tech@gmail.com>
2026-04-08 18:34:02 -0400

    fix: ntfy on darwin

--- modules/common/ntfy.nix
+++ modules/common/ntfy.nix
@@ -11,7 +11,20 @@
   }; # For NTFY notifications
 
   environment = {
-    systemPackages = [pkgs.ntfy-sh];
+    # --------------------------------------------
+    # BUGFIX: ntfy-sh does not work on Darwin
+    systemPackages = [
+      (pkgs.ntfy-sh.overrideAttrs (oldAttrs: {
+        buildInputs = (oldAttrs.buildInputs or []) ++ [pkgs.git];
+        postPatch =
+          (oldAttrs.postPatch or "")
+          + lib.optionalString pkgs.stdenv.isDarwin ''
+            substituteInPlace cmd/serve_unix.go \
+              --replace-fail '//go:build linux || dragonfly || freebsd || netbsd || openbsd' '//go:build darwin || linux || dragonfly || freebsd || netbsd || openbsd'
+          '';
+      }))
+    ];
+    # --------------------------------------------
     variables.NTFY_TOPIC = "$(cat ${config.sops.secrets."ntfy/topic".path})";
     shellAliases."ntfy" = "ntfy publish";
   };
