b73cfe56c79ce773ae070753b8e21f8060948f4e
UncleGravity <viera.tech@gmail.com>
2026-05-14 20:44:49 -0700

    nixify git + lazygit configs

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -1,18 +1,54 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: let
-  git = inputs.wrapper-manager.lib.wrapWith pkgs {
-    basePackage = pkgs.git;
-    env.GIT_CONFIG_GLOBAL.value = ./config;
-    pathAdd = [
-      pkgs.delta # Better diff viewer (configured in git config)
-      pkgs.openssh # SSH for remote repositories
-      pkgs.coreutils # Essential utilities (cp, mv, etc.) used by git
-      pkgs.git-lfs # Large File Storage support
-    ];
+{...}: {
+  programs.git = {
+    enable = true;
+    lfs.enable = true; # Large File Storage support
+    settings = {
+      user.name = "UncleGravity";
+      user.email = "viera.tech@gmail.com";
+
+      init.defaultBranch = "main";
+      color.ui = "auto";
+
+      # Pager
+      core.pager = "delta --paging=never";
+      interactive.diffFilter = "delta --color-only --features=interactive";
+      delta.side-by-side = true;
+      delta."interactive".keep-plus-minus-markers = false;
+
+      # Interactive diff tool
+      diff.tool = "nvim";
+      difftool.nvim.cmd = "nvim -d $LOCAL $REMOTE";
+
+      # Submodules
+      submodule.recurse = true; # automatically handle submodules in operations
+      status.submoduleSummary = true; # Show submodule changes in status
+
+      push = {
+        default = "simple";
+        autoSetupRemote = true; # Auto-create upstream branch on push
+        recurseSubmodules = "on-demand"; # Handle submodules on push
+      };
+
+      pull.rebase = true;
+
+      # Rerere - remember how you resolved merge conflicts
+      rerere.enabled = true;
+
+      # Rebase improvements
+      rebase = {
+        autoStash = true; # stash changes before rebase
+        autoSquash = true; # squash fixup! commits
+      };
+
+      # Better merge conflict display showing original, yours, and theirs
+      merge.conflictStyle = "zdiff3";
+    };
   };
-in {
-  home.packages = [git];
+
+  # Better diff viewer
+  # (enableGitIntegration would clobber our custom `core.pager` / `diffFilter` args).
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = false;
+  };
 }
--- modules/home/lazygit/default.nix
+++ modules/home/lazygit/default.nix
@@ -1,15 +1,29 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: let
-  lazygit = inputs.wrapper-manager.lib.wrapWith pkgs {
-    basePackage = pkgs.lazygit;
-    appendFlags = ["-ucf" ./config.yml];
-    pathAdd = [
-      pkgs.delta
-    ];
+{...}: {
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      nerdFontsVersion = "3";
+
+      # Git specific settings
+      git = {
+        autoFetch = false;
+        autoRefresh = true;
+        pagers = [
+          {
+            colorArg = "always";
+            pager = ''delta --dark --paging=never --line-numbers --hyperlinks --hyperlinks-file-link-format="lazygit-edit://{path}:{line}"'';
+          }
+        ];
+      };
+
+      gui.sidePanelWidth = 0.2;
+      refresher.refreshInterval = 0; # disables auto-refresh. really annoying, don't enable.
+      os = {
+        # OSC52 clipboard.
+        copyToClipboardCmd = ''printf "\033]52;c;$(printf {{text}} | base64 | tr -d '\n')\a" > /dev/tty'';
+        # Use nvim to edit files
+        editPreset = "nvim";
+      };
+    };
   };
-in {
-  home.packages = [lazygit];
 }
