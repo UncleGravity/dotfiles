b442e852ade9139b96bcbc27fbdf3f4149128004
UncleGravity <viera.tech@gmail.com>
2026-05-15 19:40:32 -0700

    remove tv overlay

--- overlays/television.nix
+++
@@ -1,8 +0,0 @@
-{inputs}:
-#
-final: prev: {
-  television =
-    if inputs ? television
-    then inputs.television.packages.${prev.system}.default
-    else prev.television or (throw "television not available in nixpkgs and flake input not found");
-}
