1154bd6ec878b923eaece5ea14cfcd1b161b748b
UncleGravity <viera.tech@gmail.com>
2026-09-23 18:28:49 -0700

    switch to qwen abliterated on sisyphus

--- machines/sisyphus/inference/default.nix
+++ machines/sisyphus/inference/default.nix
@@ -5,8 +5,8 @@
     enable = true;
     operators = ["angel"];
     instances.qwen = {
-      recipe = "qwen3-8-27b";
-      # recipe = "huihui-qwen3-8-27b-abliterated";
+      # recipe = "qwen3-8-27b";
+      recipe = "huihui-qwen3-8-27b-abliterated";
       autoStart = true;
     };
   };
